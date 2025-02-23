edc42bd8e5d6a45a273a540cbbdaa63f55436042
lowes2 <maxwellwang84@gmail.com>
2025-02-23 18:10:15 -0500

    you can skip cutscenes now

--- cuttingmyballsoffwithscissors.gd
+++ cuttingmyballsoffwithscissors.gd
@@ -3,11 +3,15 @@ extends Node
 var has_cutscene_played = false  # This resets when the game restarts
 @onready var theFnafMovie = $VideoStreamPlayer
 
+
 func _ready():
 	if has_cutscene_played:
 		print("Cutscene already played this session. Skipping.")
 		return
-
-	has_cutscene_played = true  # Mark cutscene as played
+	has_cutscene_played = true
 	print("Playing cutscene for the first time this session.")
 	theFnafMovie.play()
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		theFnafMovie.stop()
+		print ("Skipping cutscene.")
