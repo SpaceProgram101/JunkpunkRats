816b51b0c30796c2c01da59e8115f31ff8817a91
lowes2 <maxwellwang84@gmail.com>
2025-03-03 16:42:55 -0500

    added a rat with a staff

---
+++ rat_staff.gd
@@ -0,0 +1,123 @@
+extends CharacterBody2D
+
+
+const SPEED = 50.0
+const CHARGE_SPEED = 65
+const JUMP_VELOCITY = -400.0
+var direction = 1
+var health = 10
+var attacking = false
+var start_position = Vector2()
+var dead = false
+var can_attack = true
+
+@onready var area : Area2D = $Area2D
+@onready var laser = $laser_detection/laser
+@onready var detector = $laser_detection
+
+
+var is_player_detected = false
+
+@onready var player = get_node("/root/Node2D/Player")
+
+func _ready():
+	health = 10
+	start_position = position
+	laser.play("default")
+	laser.visible = false
+	$AnimatedSprite2D.play("idle")
+
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	# Move the enemy back and forth
+	
+	# Update position based on velocity
+	
+	# Flip sprite based on direction
+	if direction == -1 and not attacking:
+		$AnimatedSprite2D.flip_h = true # Flip sprite horizontally to face left
+	elif direction == 1 and not attacking:
+		$AnimatedSprite2D.flip_h = false  # Flip sprite horizontally to face right
+
+	# Flip direction when reaching a certain distance
+	if position.x > start_position.x + 100 and not attacking:  # Move right 200 pixels from start position
+		direction = -1  # Move left
+	elif position.x < start_position.x - 100 and not attacking: # Move left 200 pixels from start position
+		direction = 1  # Move right
+		
+	if player.position.x > position.x:
+		direction = 1
+	else:
+		direction = -1
+	if not attacking:
+		move_and_slide()	
+		
+func take_damage(damage : int):
+	if not dead:
+		health -= damage
+		if health <= 0:
+			die()
+		
+func die():
+	if not dead:
+		dead = true
+		$AnimatedSprite2D.play("dead")
+		await $AnimatedSprite2D.animation_finished
+		queue_free()
+
+
+func crash_out():
+	if player.position.x > position.x:
+		direction = 1
+	else:
+		direction = -1
+	if direction == -1:
+		$AnimatedSprite2D.flip_h = true # Flip sprite horizontally to face left
+	elif direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	
+	if is_player_detected and player and can_attack:
+		can_attack = false
+		attacking = true
+		var direction = (player.position - position).normalized()
+		$AnimatedSprite2D.play("charge")
+		await $AnimatedSprite2D.animation_finished
+		detector.rotation = direction.angle()
+		
+		
+		$AnimatedSprite2D.play("fire")
+		laser.visible = true
+		await $AnimatedSprite2D.animation_finished
+		laser.visible = false
+		attacking = false
+		
+		
+		var timer = $Timer
+		timer.wait_time = 2.0
+		timer.start()
+		await timer.timeout
+		can_attack = true
+		if is_player_detected:
+			crash_out()
+		
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
+		is_player_detected = true
+		crash_out()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
+		is_player_detected = false
+
+
+func _on_laser_detection_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.take_damage(1)
