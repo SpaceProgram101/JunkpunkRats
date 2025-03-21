cee6f6a36b66a208f39cd4c14efa21e46ecd50f4
lowes2 <maxwellwang84@gmail.com>
2025-03-20 21:47:45 -0400

    funny

---
+++ light_follow.gd
@@ -0,0 +1,20 @@
+extends PointLight2D
+
+@onready var player = get_node("/root/Node2D/Player")
+var rotation_speed = 3.0
+var rotation_offset = -PI / 2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = player.global_position - global_position
+	# Calculate the angle in radians between the sprite and the player
+	if direction.length() > 0:	
+		var target_angle = direction.angle() + rotation_offset
+	# 	Set the sprite's rotation to the calculated angle
+		rotation = lerp_angle(rotation, target_angle, rotation_speed * delta)
