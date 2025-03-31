1eac9328cb03cec657a1b8297211b436acaa6024
lowes2 <maxwellwang84@gmail.com>
2025-03-31 16:13:27 -0400

    added a clyump

--- cuttingmyballsoffwithscissors.gd
+++ cuttingmyballsoffwithscissors.gd
@@ -55,6 +55,7 @@ func death():
 	
 func _on_boss_arena_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not boss_spawned_yet:
+		boss_spawned_yet = true
 		theFnafMovie.stream = voicelines[2]
 		theFnafMovie.play()
 		player.frozen = true
--- helicopter_boss.gd
+++ helicopter_boss.gd
@@ -211,5 +211,7 @@ func die():
 	active = false
 	can_attack = false
 	dead = true
+	get_node("/root/Node2D/boss_arena/StaticBody2D/CollisionShape2D").disabled = true
+	get_node("/root/Node2D/boss_arena/StaticBody2D/Sprite2D").visible = false
 	queue_free()
 	
